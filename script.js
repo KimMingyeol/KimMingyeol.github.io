36c1e8947e048917e699b620bfc53cb748b5f566
MINGYEOL KIM <mingyeol.kim@postech.ac.kr>
2025-03-13 21:54:15 +0900

    Modify original landscape image ratio

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const tab = document.querySelector(".tab");
 
     function adjustImage() {
-        const min_landscape_image_height = 500; // MODIFY HERE if you want
+        const min_landscape_image_height = 450; // MODIFY HERE if you want
 
         const img_ratio = img.naturalWidth / img.naturalHeight;
         const width_percentage_landscape_image_covers_tab = 100;    // MODIFY HERE if you change the image's "width(%)" property in style.scss
         const min_tab_width_to_keep_img_ratio = (100/width_percentage_landscape_image_covers_tab) * (min_landscape_image_height * img_ratio);
 
         // Window size; Shrinked (landscape image cropped in width-axis) : Enlarged (landscape image expanded with img_ratio kept)
-        const current_tab_width = tab.offsetWidth;
+        const current_tab_width = tab.offsetWidth;  // As described in style.scss, 960px is the maximum tab width
         img.style.objectFit = min_tab_width_to_keep_img_ratio > current_tab_width ? "cover" : "contain";
         img.style.height = min_tab_width_to_keep_img_ratio > current_tab_width ? min_landscape_image_height.toString() + "px" : "auto";
     }
